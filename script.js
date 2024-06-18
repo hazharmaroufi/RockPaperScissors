7210529a76a0e4db77a8aefb3527032d9f031629
hazharmaroufi <hazhmar@gmail.com>
2024-06-19 01:30:16 +0330

    PlayRound function coded

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+let humanScore = 0;
+let computerScore = 0;
 let choises = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
   const rnd = Math.floor((Math.random() * 3));
   // console.log(rnd);
   console.log(choises[rnd]);
+  return (choises[rnd]);
 }
-getComputerChoice();
+// getComputerChoice();
 
 function getHumanChoice() {
-  const hChoise = prompt("rock, paper, scissors");
+  const hChoise = prompt("rock, paper, scissors").toLowerCase();
   console.log(hChoise);
+  return (hChoise);
 }
-getHumanChoice();
+// getHumanChoice();
+
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "rock") {
+    console.log("draw");
+  } else if (humanChoice == "paper" && computerChoice == "paper") {
+    console.log("draw");
+  } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+    console.log("draw");
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You lose! Paper beats Rock");
+    computerScore++
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win! Rock beats Scissors");
+    humanScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win! scissors beats paper");
+    humanScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("You lost! rock beats scissors");
+    computerScore++;
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("You lost! paper beats scissors");
+    computerScore++;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You win! paper beats rock");
+    humanScore++;
+  }
+
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(computerScore);
+console.log(humanScore);
